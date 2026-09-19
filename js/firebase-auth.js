9058d37b4c86676dee866e0191c57871dea70cb7
Ashish Pandey <ap965599@gmail.com>
2026-09-19 17:43:05 +0530

    Add Firebase authentication foundation

---
+++ js/firebase-auth.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_i3OuD8zEnmsTjobg1yUt9zSHZAYb4ug",
+  authDomain: "gifty-hamper.firebaseapp.com",
+  projectId: "gifty-hamper",
+  storageBucket: "gifty-hamper.firebasestorage.app",
+  messagingSenderId: "941922060172",
+  appId: "1:941922060172:web:23d85fd867bec63b7a78a9",
+  measurementId: "G-F18T3N12CK"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+  auth,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut
+};

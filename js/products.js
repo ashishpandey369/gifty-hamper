7924f5fdcf3458b1c5623d6e3ef3bbba142497e0
Ashish Pandey <ap965599@gmail.com>
2026-09-20 16:28:28 +0530

    Refresh shop shared catalog cache

--- js/products.js
+++ js/products.js
@@ -24,7 +24,7 @@ import {
   DEFAULT_CATALOG,
   getDiscountPercent,
   loadPublicCatalog
-} from "./catalog-store.js";
+} from "./catalog-store.js?v=2";
 
 let GIFTS = DEFAULT_CATALOG.map(product => ({ ...product }));
 
